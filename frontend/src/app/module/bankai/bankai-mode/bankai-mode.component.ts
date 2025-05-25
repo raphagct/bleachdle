35d25c96ec9ef47bfebc294a727a5b6f8a30aa83
Guichet Raphaël <raphael.guichet@etu.unice.fr>
2025-05-25 18:51:48 +0200

    +bankai mode

--- frontend/src/app/module/bankai/bankai-mode/bankai-mode.component.ts
+++ frontend/src/app/module/bankai/bankai-mode/bankai-mode.component.ts
@@ -3,11 +3,19 @@ import {CharacterService} from '../../../service/character.service';
 import {Character} from '../../../model/character';
 import {SearchBarComponent} from '../../../shared/search-bar/search-bar.component';
 import {Bankai} from '../../../model/bankai';
+import {NgClass, NgForOf, NgIf} from '@angular/common';
+import {RouterLink} from '@angular/router';
+import {Panel} from 'primeng/panel';
 
 @Component({
   selector: 'app-bankai-mode',
   imports: [
-    SearchBarComponent
+    SearchBarComponent,
+    NgForOf,
+    NgIf,
+    RouterLink,
+    Panel,
+    NgClass
   ],
   templateUrl: './bankai-mode.component.html',
   styleUrl: './bankai-mode.component.scss'
@@ -15,6 +23,10 @@ import {Bankai} from '../../../model/bankai';
 export class BankaiModeComponent implements OnInit{
   characters: Character[] = [];
   bankaiToGuess!: Bankai;
+  selectedCharacters: Character[] = [];
+  isFind : boolean = false;
+  tries : number = 0;
+
   constructor(private characterService: CharacterService) {}
 
   ngOnInit(): void {
@@ -24,6 +36,22 @@ export class BankaiModeComponent implements OnInit{
     this.characterService.getDailyBankai().subscribe(data => {
       this.bankaiToGuess = data;
     });
+  }
+
+  onCharacterSelected(character: Character) {
+    this.selectedCharacters.push(character);
+    this.tries++;
+    if (character.id == this.bankaiToGuess.personnage.id){
+      this.isFind = true;
+    }
+  }
+
+  getAttributeStatus(character : Character): 'match' | 'partial' | 'miss' {
+    if(character.id === this.bankaiToGuess.personnage.id){
+      return 'match';
+    }else{
+      return 'miss';
+    }
   }
 
 
